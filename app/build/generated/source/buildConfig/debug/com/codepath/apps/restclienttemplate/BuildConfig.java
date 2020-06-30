4744574660ec4efd13dcbc770f1675b7ea3e30a1
Maxine Nzegwu <mnzegwu@fb.com>
2020-06-30 14:16:30 -0400

    api request and initial models

---
+++ app/build/generated/source/buildConfig/debug/com/codepath/apps/restclienttemplate/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codepath.apps.restclienttemplate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codepath.apps.restclienttemplate";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String CONSUMER_KEY = "6l5fgXaV0A7TOPqLXI2jf51g2";
+  public static final String CONSUMER_SECRET = "kDaZT9V76vvNjdu270ikJxaTXQrJT0ibw8a4wBxB31nPGB2Ruq";
+}
